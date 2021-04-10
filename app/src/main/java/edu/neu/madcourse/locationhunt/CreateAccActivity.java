7ff411574cc217c915798036180f1e6b61562c44
crileyy <criley0007@gmail.com>
2021-04-10 15:11:20 -0400

    home activity and navigation from login and create acc

--- app/src/main/java/edu/neu/madcourse/locationhunt/CreateAccActivity.java
+++ app/src/main/java/edu/neu/madcourse/locationhunt/CreateAccActivity.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.locationhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateAccActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CreateAccActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
+    }
+
+    public void createAccount(View view) {
+        // TODO need firebase(?) code for creating account
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/neu/madcourse/locationhunt/LoginActivity.java
+++ app/src/main/java/edu/neu/madcourse/locationhunt/LoginActivity.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.locationhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        // TODO need firebase(?) code for logging in
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }

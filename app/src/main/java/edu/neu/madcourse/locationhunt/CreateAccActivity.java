d43342ffb720f1503d9067abf3b47583b70c77b4
John Philip <john.philip@buzzfeed.com>
2021-04-19 16:13:49 -0400

    Adding google service config file

--- app/src/main/java/edu/neu/madcourse/locationhunt/CreateAccActivity.java
+++ app/src/main/java/edu/neu/madcourse/locationhunt/CreateAccActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class CreateAccActivity extends AppCompatActivity {
 
+    private final String SERVER_KEY = "key=AAAAZ5n7XB8:APA91bFrywGMYLKWVCR7dwa3U2x7MdAqeXsF8fzsTcRoPvVV3C2EBCzOjgAd6db37IlI4ZI-SQLBHFyud5-ny3Ev3eCJr4sdKt_XVTCva-im9iR0dpwmwsMhhTwPkeIQiYn9EykdbvC_";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

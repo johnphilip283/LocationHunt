1f0f4680b38e56c5e0d54f917324c9ad21ac86ea
John Philip <john.philip@buzzfeed.com>
2021-04-21 22:46:07 -0400

    add fake location data.

--- app/src/main/java/edu/neu/madcourse/locationhunt/InitialStreetViewActivity.java
+++ app/src/main/java/edu/neu/madcourse/locationhunt/InitialStreetViewActivity.java
@@ -5,13 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class InitialStreetViewActivity extends AppCompatActivity {
+
+    private SeekBar distanceSeekBar;
+    private TextView distanceFilterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_street_view);
+
+        distanceSeekBar = findViewById(R.id.distance_seek_bar);
+        distanceFilterText = findViewById(R.id.distance_filter_text);
+
+        distanceFilterText.setText(getString(R.string.distance_filter_text, distanceSeekBar.getProgress() + ""));
+
+        distanceSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                distanceFilterText.setText(getString(R.string.distance_filter_text, progress + ""));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     public void startHunt(View view) {

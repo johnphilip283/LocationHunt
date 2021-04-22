5f45c33f9f60e9516ddc1083eb9802fb20a0883a
crileyy <criley0007@gmail.com>
2021-04-21 21:32:49 -0400

    location tracking working

--- app/src/main/java/edu/neu/madcourse/locationhunt/GameActivity.java
+++ app/src/main/java/edu/neu/madcourse/locationhunt/GameActivity.java
@@ -2,17 +2,32 @@ package edu.neu.madcourse.locationhunt;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GameActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    MapView mapView;
+    private MapView mapView;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +37,13 @@ public class GameActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.onCreate(savedInstanceState);
 
         mapView.getMapAsync(this);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        getLocationUpdates(googleMap);
+        startLocationUpdates();
     }
 
     @Override
@@ -69,6 +86,45 @@ public class GameActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
+    }
+
+    private void getLocationUpdates(GoogleMap gMap) {
+        this.locationRequest = new LocationRequest();
+        this.locationRequest.setInterval(30000);
+        this.locationRequest.setFastestInterval(20000);
+        this.locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        this.locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (!locationResult.getLocations().isEmpty()) {
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+                        gMap.addMarker(markerOptions);
+                        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
+                    }
+                }
+            }
+        };
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        this.fusedLocationClient.requestLocationUpdates(
+                this.locationRequest,
+                this.locationCallback,
+                null);
     }
 
 }
